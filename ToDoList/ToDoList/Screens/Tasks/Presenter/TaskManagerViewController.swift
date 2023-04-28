3a069fa36632f0dbdd2fd2a799fe32986f02ccf2
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-04-28 08:52:22 +0000

    Created from xib file view for the TaskViewController, organized structure for TaskTableView, appended test 10 cells, added constraints to tableview, setted up navigation bar appearance, extended with delegations table view's functionality

--- ToDoList/ToDoList/Screens/Tasks/Presenter/TaskManagerViewController.swift
+++ ToDoList/ToDoList/Screens/Tasks/Presenter/TaskManagerViewController.swift
@@ -8,22 +8,85 @@
 import UIKit
 
 class TaskManagerViewController: UIViewController {
+    
+    @IBOutlet weak var taskTableView: UITableView!
+    
+    private func initialSetup(){
+        Bundle.main.loadNibNamed("TaskViewController", owner: self,options: nil)
+    }
+    
+    private func setupNavigationBar(){
+        
+        title = "Task Manager"
+        
+        let customBarAppearance = UINavigationBarAppearance()
+        
+        customBarAppearance.configureWithOpaqueBackground()
+        customBarAppearance.backgroundColor = .clear
+        
+        customBarAppearance.titleTextAttributes = [
+            NSAttributedString.Key.font:UIFont.systemFont(
+                ofSize: 22,
+                weight: .bold
+            )
+        ]
+        
+        navigationItem.standardAppearance = customBarAppearance
+        navigationItem.compactAppearance = customBarAppearance
+        navigationItem.scrollEdgeAppearance = customBarAppearance
+        
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
+    }
+    
+    private func setupTaskTableView(){
+        view.addSubview(taskTableView)
+        
+        taskTableView.translatesAutoresizingMaskIntoConstraints = false
+        taskTableView.backgroundColor = .gray
+        
+        taskTableView.delegate = self
+        taskTableView.dataSource = self
+        
+        taskTableView.register(
+            UINib(
+            nibName: "TaskTableViewCell",
+            bundle: nil
+            ),
+            forCellReuseIdentifier: "taskCell")
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initialSetup()
 
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
+        
+        setupTaskTableView()
+        
     }
+    
+ 
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension TaskManagerViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell")
+        
+        return cell ?? UITableViewCell(frame: .zero)
+        
+    }
+    
+    
+    
 }

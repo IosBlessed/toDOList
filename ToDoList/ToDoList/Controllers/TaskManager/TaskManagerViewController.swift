eceea7e43ff6fe92ec7ed66415037809df2fa367
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-04-28 23:28:49 +0300

    Null-ed table view's cells

--- ToDoList/ToDoList/Controllers/TaskManager/TaskManagerViewController.swift
+++ ToDoList/ToDoList/Controllers/TaskManager/TaskManagerViewController.swift
@@ -44,7 +44,7 @@ final class TaskManagerViewController: UIViewController {
 extension TaskManagerViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {

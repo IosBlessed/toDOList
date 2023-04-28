9afeb37eed3779c467d91d88d3a07346b489f727
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-04-28 11:28:06 +0000

    New group for extensions added

--- ToDoList/ToDoList/Screens/Tasks/Extensions/Extension + TaskManagerViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Extension + TaskManagerViewController.swift
-//  ToDoList
-//
-//  Created by Никита Данилович on 28.04.2023.
-//
-
-import Foundation
---
+++ ToDoList/ToDoList/Screens/Tasks/Extensions/Extension+TaskManagerViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+extension TaskManagerViewController:UITableViewDelegate,UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView(frame: .zero)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+ 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TaskTableViewCell.identifier)
+        return cell ?? UITableViewCell(frame: .zero)
+    }
+    
+}

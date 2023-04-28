237a68472c0e944de8280b886d34549b4d5d475c
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-04-28 23:28:20 +0300

    Linted empty spaces and removed useless functions

--- ToDoList/ToDoList/Classes/SceneDelegate.swift
+++ ToDoList/ToDoList/Classes/SceneDelegate.swift
@@ -6,16 +6,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+
         let window = UIWindow(windowScene: windowScene)
+
         let taskViewController = TaskManagerViewController(
             nibName: String(describing: TaskManagerViewController.self),
             bundle: nil
         )
+
         let taskNavigationController = UINavigationController(rootViewController: taskViewController)
+
         window.rootViewController = taskNavigationController
         window.makeKeyAndVisible()
+
         self.window = window
     }
+
     func sceneDidEnterBackground(_ scene: UIScene) {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }

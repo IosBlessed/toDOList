958d9b6e9c4dadefc541ccbc6b554c49f042e844
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-04-28 17:25:45 +0300

    Created structs for designs inside app(based on colors and fonts)

--- ToDoList/ToDoList/Materials/Design/DesignSystem.swift
+++ ToDoList/ToDoList/Materials/Design/DesignSystem.swift
@@ -1,8 +1,22 @@
-//
-//  DesignSystem.swift
-//  ToDoList
-//
-//  Created by Никита Данилович on 28.04.2023.
-//
+import UIKit
 
-import Foundation
+struct DesignedSystemColors {
+    let primary: UIColor
+    let accent: UIColor
+    let textPrimary: UIColor
+    let textSubtitle: UIColor
+    let contrast: UIColor
+}
+
+struct DesignedSystemFonts {
+    let headline: UIFont
+    let body: UIFont
+    let button: UIFont
+    
+    init(){
+        self.headline = UIFont.systemFont(ofSize: 24, weight: .bold)
+        self.body = UIFont.systemFont(ofSize: 16, weight: .regular)
+        self.button = UIFont.systemFont(ofSize: 18, weight: .bold)
+    }
+    
+}

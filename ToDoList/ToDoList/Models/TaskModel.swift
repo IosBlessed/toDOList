c0fd9a635add8670bf46685bc0399575c36f30cf
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-02 15:05:27 +0300

    Created status for each section(active/completed) and array of tasks inside the structure of a task's section

--- ToDoList/ToDoList/Models/TaskModel.swift
+++ ToDoList/ToDoList/Models/TaskModel.swift
@@ -1,12 +1,16 @@
 import UIKit
 
-enum TaskSection: String {
+enum TaskSectionStatus: String {
     case active = "Active"
     case completed = "Completed"
 }
 
-struct Task: Hashable {
-    let section: TaskSection
+struct TaskSection: Hashable {
+    let status: TaskSectionStatus
+    let tasks: [TaskModel]
+}
+
+struct TaskModel: Hashable {
     let title: String
-    let description: String
+    let description: String?
 }

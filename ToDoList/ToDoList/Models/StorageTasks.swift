5246a0bf6a881d735a45e4a0a3a831d5a8d535c5
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-02 17:53:36 +0300

    Created storage for data keeping (tasks) and realisation via Model

--- ToDoList/ToDoList/Models/StorageTasks.swift
+++ ToDoList/ToDoList/Models/StorageTasks.swift
@@ -1,8 +1,48 @@
-//
-//  StorageTasks.swift
-//  ToDoList
-//
-//  Created by Никита Данилович on 02.05.2023.
-//
+import UIKit
 
-import Foundation
+class StorageTasks {
+
+    private var sections = [TaskSection]()
+    private var activeTasks = [TaskModel]()
+    private var completedTasks = [TaskModel]()
+
+    func addTask(status: TaskStatus, title: String, description: String?) {
+        switch status {
+        case .active:
+            activeTasks.append(TaskModel(status: status, title: title, description: description))
+        case .completed:
+            completedTasks.append(TaskModel(status: status, title: title, description: description))
+        }
+    }
+
+    func setSections() {
+        if !activeTasks.isEmpty {
+            sections.append(TaskSection(title: .active, tasks: activeTasks))
+        }
+        if !completedTasks.isEmpty {
+            sections.append(TaskSection(title: .completed, tasks: completedTasks))
+        }
+    }
+
+    func getSections() -> [TaskSection]? {
+        return sections
+    }
+
+    init() {
+            addTask(status: .active, title: "First task", description: nil)
+            addTask(status: .completed, title: "Second task", description: nil)
+            addTask(status: .completed, title: "Third task", description: nil)
+            addTask(status: .active, title: "Fourth task", description: nil)
+            addTask(status: .completed, title: "Fifth task", description: nil)
+
+            setSections()
+    }
+
+    func getCompletedTasks() -> [TaskModel]? {
+        return completedTasks
+    }
+
+    func getActiveTasks() -> [TaskModel]? {
+        return activeTasks
+    }
+}

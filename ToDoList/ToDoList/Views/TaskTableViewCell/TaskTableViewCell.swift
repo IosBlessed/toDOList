b963321592dc9cb716184d9e56452b223d23f18a
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-02 15:06:30 +0300

    Added label's outlet for table view's cell where will be deisplayed task's title

--- ToDoList/ToDoList/Views/TaskTableViewCell/TaskTableViewCell.swift
+++ ToDoList/ToDoList/Views/TaskTableViewCell/TaskTableViewCell.swift
@@ -1,7 +1,8 @@
 import UIKit
 
 class TaskTableViewCell: UITableViewCell {
-    
+
+    @IBOutlet weak var taskTitle: UILabel!
     static let nib: UINib = {
         return UINib(
             nibName: String(describing: TaskTableViewCell.self),
@@ -17,6 +18,4 @@ class TaskTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-    
-    
 }

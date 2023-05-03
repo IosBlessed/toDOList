c04e880442eab776ffa27652aad8b15691577ae7
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-03 16:53:19 +0300

    Renamed presenter's protocol, initialization via injection + empty spaces

--- ToDoList/ToDoList/Presenters/TaskManagerPresenterImp.swift
+++ ToDoList/ToDoList/Presenters/TaskManagerPresenterImp.swift
@@ -1,11 +1,16 @@
-final class TaskManagerPresenterImp: TaskManagerPresenterProtocol {
+import UIKit
+final class TaskManagerPresenterImp: TaskManagerPresenter {
 
     weak var taskManagerViewController: TaskManagerViewController?
-
-    private var storage: StorageTasks? = StorageTasks()
+    var storage: Storage?
 
     func requestSections() {
         guard let sections = storage?.getSections() else {return}
         taskManagerViewController?.updateTaskManagerViewController(with: sections)
+    }
+
+    init(viewController: TaskManagerViewController, storage: Storage?) {
+        self.taskManagerViewController = viewController
+        self.storage = storage
     }
 }

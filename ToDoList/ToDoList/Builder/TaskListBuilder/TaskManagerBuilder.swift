d6dc350f6c7bcd10dc8c9a4f0624936dfaf99194
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-03 16:46:26 +0300

    Skeleton for builder of the project

--- ToDoList/ToDoList/Builder/TaskListBuilder/TaskManagerBuilder.swift
+++ ToDoList/ToDoList/Builder/TaskListBuilder/TaskManagerBuilder.swift
@@ -1,8 +1,29 @@
-//
-//  TaskManagerBuilder.swift
-//  ToDoList
-//
-//  Created by Никита Данилович on 03.05.2023.
-//
+import UIKit
 
-import Foundation
+final class TaskManagerBuilder {
+
+    static let shared = TaskManagerBuilder()
+
+    private var taskManagerVC: TaskManagerViewController?
+    private var taskManagerPresenter: TaskManagerPresenter? {
+        didSet {
+            taskManagerVC?.taskManagerPresenter = taskManagerPresenter
+        }
+    }
+    private var storage: Storage?
+
+    private init() {}
+
+    func buildTaskManager() -> TaskManagerViewControllerImp? {
+        storage = StorageImp()
+        taskManagerVC = TaskManagerViewControllerImp(
+            nibName: String(describing: TaskManagerViewControllerImp.self),
+            bundle: nil
+        )
+        taskManagerPresenter = TaskManagerPresenterImp(
+            viewController: taskManagerVC!,
+            storage: storage!
+        )
+        return taskManagerVC as? TaskManagerViewControllerImp
+    }
+}

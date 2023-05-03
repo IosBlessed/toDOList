28771bdf225879f63eddfd48f4926415808130ec
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-03 16:48:17 +0300

    Added section with cell's separator

--- ToDoList/ToDoList/Controllers/TaskTableViewCell/TaskTableViewCell.swift
+++ ToDoList/ToDoList/Controllers/TaskTableViewCell/TaskTableViewCell.swift
@@ -9,6 +9,15 @@ class TaskTableViewCell: UITableViewCell {
             bundle: nil
         )
     }()
+    private lazy var separatorCell: UIView = {
+        let separator = UIView(frame: .zero)
+        separator.translatesAutoresizingMaskIntoConstraints = false
+        separator.backgroundColor = .gray.withAlphaComponent(Constants.taskTableViewCellSeparatorAlphaColor)
+        separator.layer.masksToBounds = true
+        separator.layer.cornerRadius = 2
+
+        return separator
+    }()
     static let identifier: String = "taskCell"
 
     override func awakeFromNib() {
@@ -17,5 +26,19 @@ class TaskTableViewCell: UITableViewCell {
 
     func setupCell(title: String) {
         taskTitle.text = title
+        configureSeparator()
+    }
+
+    private func configureSeparator() {
+        self.addSubview(separatorCell)
+
+        separatorCell.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        separatorCell.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 50).isActive = true
+        separatorCell.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -2).isActive = true
+        separatorCell.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -2).isActive = true
+    }
+
+    func removeSeparator() {
+        separatorCell.removeFromSuperview()
     }
 }

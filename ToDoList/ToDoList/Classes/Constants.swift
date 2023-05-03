df1bf52f101546ef8b81ac651783cd2eeabdbf50
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-03 16:46:56 +0300

    Added constant for alpha background of cell's separator

--- ToDoList/ToDoList/Classes/Constants.swift
+++ ToDoList/ToDoList/Classes/Constants.swift
@@ -3,4 +3,6 @@ import UIKit
 struct Constants {
     static let navigationBarTitleFontSize: CGFloat = 22.0
     static let taskTableViewHeightForRow: CGFloat = 60.0
+    static let taskTableViewHeaderFooterTitleSize: CGFloat = 20.0
+    static let taskTableViewCellSeparatorAlphaColor: CGFloat = 0.3
 }

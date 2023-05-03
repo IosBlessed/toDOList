e073a0536885a92a4a6f8eb0f7b8f39f21660bb3
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-03 16:52:13 +0300

    Added custom header to the table view's sections

--- ToDoList/ToDoList/Controllers/TaskTableViewHeaderFooterView/TaskTableViewHeaderFooterView.swift
+++ ToDoList/ToDoList/Controllers/TaskTableViewHeaderFooterView/TaskTableViewHeaderFooterView.swift
@@ -9,12 +9,38 @@ import UIKit
 
 class TaskTableViewHeaderFooterView: UITableViewHeaderFooterView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    static let identifier = "headerFooterView"
 
+    lazy var sectionTitle: UILabel = {
+        let sectionTitle = UILabel(frame: .zero)
+        sectionTitle.translatesAutoresizingMaskIntoConstraints = false
+        sectionTitle.attributedText = NSAttributedString(
+            string: sectionTitle.text ?? " ",
+            attributes: [
+                NSAttributedString.Key.font: UIFont.systemFont(
+                    ofSize: Constants.taskTableViewHeaderFooterTitleSize,
+                    weight: .bold
+                )
+            ]
+        )
+        return sectionTitle
+    }()
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        configureContents()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureContents() {
+        self.addSubview(sectionTitle)
+
+        sectionTitle.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        sectionTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+        sectionTitle.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30).isActive = true
+        sectionTitle.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5).isActive = true
+    }
 }

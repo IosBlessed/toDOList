6c669cefbf9693286be7992b95307ddeffe26c7c
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-04 18:28:52 +0300

    Added additional fonts of san francisco's style

--- ToDoList/ToDoList/Classes/Constants.swift
+++ ToDoList/ToDoList/Classes/Constants.swift
@@ -1,8 +1,10 @@
 import UIKit
 
 struct Constants {
-    static let navigationBarTitleFontSize: CGFloat = 22.0
-    static let taskTableViewHeightForRow: CGFloat = 60.0
-    static let taskTableViewHeaderFooterTitleSize: CGFloat = 20.0
+    static let taskTableViewHeightForRow: CGFloat = 52.0
     static let taskTableViewCellSeparatorAlphaColor: CGFloat = 0.3
+    static let sanFranciscoBold: UIFont = UIFont(name: "SFProDisplay-Bold", size: 24) ??
+                                          UIFont.systemFont(ofSize: 24, weight: .bold)
+    static let sanFranciscoRegular: UIFont = UIFont(name: "SFProDisplay-Regular", size: 16) ??
+                                             UIFont.systemFont(ofSize: 16, weight: .regular)
 }

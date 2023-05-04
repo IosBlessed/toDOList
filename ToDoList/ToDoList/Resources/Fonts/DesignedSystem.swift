3312667ee2dfcf6b855421e89767dbf6baf974ad
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-04 18:40:11 +0300

    Added constants for DesignedSystemColor and constants for DesignedSystemFonts

--- ToDoList/ToDoList/Resources/Fonts/DesignedSystem.swift
+++ ToDoList/ToDoList/Resources/Fonts/DesignedSystem.swift
@@ -1,15 +1,22 @@
 import UIKit
 
-struct DesigneSystemFonts {
+struct DesignedSystemFonts {
     static let headline: UIFont = Constants.sanFranciscoBold.withSize(24)
     static let body: UIFont = Constants.sanFranciscoBold.withSize(16)
+    static let subtitle: UIFont = Constants.sanFranciscoRegular.withSize(16)
     static let button: UIFont = Constants.sanFranciscoBold.withSize(18)
 }
 
-struct DesigneSystemColors {
-    static let primary = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-    static let accent = UIColor(red: 0.831, green: 0.31, blue: 0.31, alpha: 1).cgColor
-    static let textPrimary = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
-    static let textSubtitle = UIColor(red: 0.565, green: 0.565, blue: 0.565, alpha: 1).cgColor
-    static let contrast = UIColor(red: 0.942, green: 0.942, blue: 0.942, alpha: 1).cgColor
+struct DesignedSystemColors {
+    static let primary = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    static let accent = UIColor(red: 0.831, green: 0.31, blue: 0.31, alpha: 1)
+    static let textPrimary = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+    static let textSubtitle = UIColor(red: 0.567, green: 0.567, blue: 0.567, alpha: 1.0)
+    static let contrast = UIColor(red: 0.942, green: 0.942, blue: 0.942, alpha: 1)
+    static let separatorTableViewCellColor = UIColor(
+        red: 0.854,
+        green: 0.854,
+        blue: 0.854,
+        alpha: 1
+    ).cgColor
 }

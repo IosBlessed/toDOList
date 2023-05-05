0f41be3d1d5a72351fb875dd44b8cc02ee50ddce
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-05-05 17:55:55 +0300

    Formed initial builder file for add task controller

--- ToDoList/ToDoList/Modules/AddTask/Builder/AddTaskBuilder.swift
+++ ToDoList/ToDoList/Modules/AddTask/Builder/AddTaskBuilder.swift
@@ -5,4 +5,21 @@
 //  Created by Никита Данилович on 05.05.2023.
 //
 
-import Foundation
+import UIKit
+
+class AddTaskBuilder {
+ 
+    static let shared = AddTaskBuilder()
+    
+    
+    func buildAddTask() -> AddTaskViewController {
+        
+        let addTaskVC = AddTaskViewController(
+            nibName: String(describing: AddTaskViewController.self),
+            bundle: nil
+        )
+        
+        return addTaskVC
+    }
+    
+}
